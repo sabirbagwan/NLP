e4c59303baec02bab7a1676c70ec15e1cfebb699
Sabir Bagwan <106504464+sabirbagwan@users.noreply.github.com>
2023-05-04 21:17:44 +0530

    Update remove_punctuation.py

--- remove_punctuation.py
+++ remove_punctuation.py
@@ -3,3 +3,14 @@ import string
 exclude = string.punctuation
 def remove_punc(text):
     return text.translate(str.maketrans('', '', exclude))
+
+
+############### 2nd 
+import string
+
+exclude = string.punctuation
+
+def remove_punc(text):
+    if not isinstance(text, str):
+        text = str(text)
+    return text.translate(str.maketrans('', '', exclude))
